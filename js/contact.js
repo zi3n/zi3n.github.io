42be2e994abc9d42b4604f301ca01b4e6bb6db82
diennd <ndanhdien12498@gmail.com>
2022-07-18 13:31:54 +0700

    update

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-function sendMail() {
-    var link = "mailto:ndanhdien98@gmail.com"
-             + "&subject=" + encodeURIComponent(document.getElementById('name').value)
-             + "&body=" + encodeURIComponent(document.getElementById('message').value)
-    ;
-    
-    window.location.href = link;
-}
